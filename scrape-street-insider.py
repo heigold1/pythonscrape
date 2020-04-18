8b2b0ef8a9ed2d1de118a3f15372fcbc565184f1
Brent Heigold <brent@heigoldinvestments.com>
2020-04-18 03:31:55 +0000

    Starting to work on rotating proxies for StreetInsider scrape

--- scrape-street-insider.py
+++ scrape-street-insider.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-
-from lxml import html
+#!/usr/bin/pythoption:'ascii' codec can't encode character '\xa9' in position 134670 lxml import html
 import requests
 from time import sleep
 import json
@@ -11,8 +9,24 @@ import sys
 import re 
 import random 
 
+from itertools import cycle
+import traceback
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            #Grabbing IP and corresponding PORT
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
 
 
 def parse_finance_page(symbol):
@@ -24,38 +38,36 @@ def parse_finance_page(symbol):
           "Connection":"keep-alive",
           "Host":"www.streetinsider.com",
           "Referer":"https://www.streetinsider.com",
+#          "Host":"finance.yahoo.com",
+#          "Referer":"https://finance.yahoo.com",
+
+
 
           "Upgrade-Insecure-Requests":"1",
           "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"
     } 
 
 
-#  print("Content-type: text/html\n\n")
-
   for retries in range(1):
     try:    
 
       url = "https://www.streetinsider.com/stock_lookup.php?LookUp=Get+Quote&q=" + symbol
+#      url = "https://finance.yahoo.com/quote/" + symbol + "?p=" + symbol
 
-      response = requests.get(url, headers = headers, verify=False)
+      response = requests.get(url, headers = headers, verify=False) 
 
       if response.status_code!=200:
         raise ValueError("Invalid Response Received From Webserver")
 
 
-#      randomFloat = randint(1,2) + random.random()
-#      print("randomFloat is " + str(randomFloat)) 
-#      sys.exit() 
 
       sleep(randint(1,3) + random.random())
 
-      #parser = html.fromstring(response.text)
-
-      responseText = response.text.encode('utf-8') 
-
-#      print(responseText) 
+      responseText = response.text
 
       responseText = re.sub(r"\$jq\('#blocker'\).fadeIn\(500\);", "console.log('nothing')", responseText)  
+
+      responseText = responseText.encode('ascii') 
 
       print(responseText) 
 
